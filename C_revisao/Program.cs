11355a0b0ab1e8171b597d3277c51366430567e6
Caua.gpereira2 <Caua.gpereira2@L130635507W10-1>
2025-02-26 22:01:26 -0300

    Adicionar arquivos de projeto.

---
+++ C_revisao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization;
+
+namespace C_revisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resutado;
+
+            Console.Write("Informe sua primeira nota: ");
+            double nota = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe sua segunda nota: ");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+
+            double soma = (nota + nota1) / 2;
+
+            if (soma >= 7)
+            {
+                resutado = "Aprovado";
+            }
+            else if (soma < 5)
+            {
+                resutado = "Reprovado";
+            }
+            else {
+                resutado = "Recuperação";
+            }
+
+            Console.WriteLine("A sua nota foi : {0}! Seu resultado final foi: {1}", soma, resutado);
+        }
+    }
+}
