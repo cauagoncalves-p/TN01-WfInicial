31aaec4059e0a52c463051fee16dd94dc224ca09
CAUA GONCALVES PEREIRA <caua.gpereira2@senacsp.edu.br>
2025-02-27 22:22:15 -0300

    Ex03

---
+++ Cex3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Cex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1º Numero");
+            int num1 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Digite o 2º Numero");
+            int num2 = int.Parse(Console.ReadLine()!);
+
+            int maior = Math.Max(num1, num2);
+            Console.WriteLine("O maior numero é o {0}", maior);
+        }
+    }
+}
